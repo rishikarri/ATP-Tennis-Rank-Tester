9867987a844d2ddf632fa8c9c23cb699cea37bbb
Rishi Karri <rishikarri1@gmail.com>
2017-02-08 15:37:56 -0500

    testKnowledge added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,99 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var config = require('../config/config.js');
+
+
+var playerRankingArray = []
+
+var connection = mysql.createConnection({
+	host: config.host, 
+	user: config.user,
+	password: config.password,
+	database: config.database
+})
+
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
 });
 
+router.post('/addNewPlayer', (req, res, next)=>{
+	// res.send('Cheers! Your player has been added to the database');
+	// res.send(req.body.playerFirstName);
+	// var insertQuery = "INSERT INTO tasks (task_name, task_date) VALUES ('"+newTask+"','"+taskDate+"')";
+	playerFirstName = req.body.playerFirstName;
+	playerLastName = req.body.playerLastName;
+	playerRank = req.body.playerRank;
+
+	var insertQuery = "INSERT INTO players_rankings (player_first_name, player_last_name, player_ranking) VALUES ('"+playerFirstName+"','"+playerLastName+"','"+playerRank+"')";
+
+	// res.send(insertQuery);
+
+	connection.query(insertQuery, (error, results, field)=>{
+		if (error) throw error;
+		res.redirect('/');
+
+	});
+
+});
+
+router.get('/testKnowledge/correctAnswer', (req, res, next)=>{
+	res.redirect('/testKnowledge');
+	var imageId = req.params.imageId;
+
+	console.log(imageId);
+})
+
+router.get('/testKnowledge/wrongAnswer' ,(req, res, next)=>{
+	res.send('wrong');
+})
+
+
+router.get('/testKnowledge', (req, res, next)=>{
+
+
+	// var getImagesQuery = "SELECT imageId FROM players_rankings";
+
+	var selectQuery = "SELECT * FROM players_rankings";	
+	connection.query(selectQuery, (error, results, fields)=>{
+		// res.json(results);
+
+		var indexToPull = 4;
+
+		//logic for wrong answer
+		
+		var wrongRank =  Math.floor(10 * Math.random()) + 1;
+
+		while(wrongRank !===indexToPull){
+			wrongRank =  Math.floor(10 * Math.random()) + 1;			
+		}		
+		res.render('testKnowledge',
+			{
+				imageToRender: '/images/'+results[indexToPull].imageId,
+				playerNameToRender: results[indexToPull].player_first_name + " " + results[indexToPull].player_last_name,
+				playerRankToRender: results[indexToPull].player_ranking,
+				wrongRankToRender: wrongRank
+
+			}
+		)
+
+	})
+	//get the current information from the database
+
+	// var selectQuery = "SELECT * FROM players_rankings";
+	// // res.send(selectQuery);
+	// connection.query(selectQuery, (req, tennisRankingData, next)=>{
+	// 	playerRankingsArray = tennisRankingData;
+	// 	res.render('testKnowledge', {playerRankingArrayKey: playerRankingArray})
+	// });
+	// res.render('testKnowledge', {});
+});
+
 module.exports = router;
+
+
+// query to eliminate imags that have already been shown on the page 
