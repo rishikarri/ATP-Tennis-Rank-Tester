9e040a0a97c31cfea2f40a0731618a7dafc65b5d
Rishi Karri <rishikarri1@gmail.com>
2017-02-09 11:25:28 -0500

    learning multer module

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var mysql = require('mysql');
 var config = require('../config/config.js');
 
 
+
 var playerRankingArray = []
 var score = 0; 
 
@@ -16,10 +17,41 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+//include Multer Module 
+var multer  = require('multer');
+var upload  = multer({dest: 'public/images'})
+// specify the type for use later, it comes from upload
+var type = upload.single('imageToUpload');
+// we will need fs to read the file, it's part of the core
+var fs = require('fs');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
   res.render('index', { title: 'Express' });
+});
+
+router.get('/uploadImage', (req, res, next)=>{
+	res.render('uploadImage', {});
+});
+
+router.post('/formSubmit', type, (req, res, next)=>{
+	// Save the path where the ffile is at temporarily
+	var tempPath = req.file.path; 
+	// Set up the target path + the orig name of the file/
+	var targetPath = 'public/images/' + req.file.originalname;
+	fs.readFile(tempPath, (error, fileContents)=>{
+		fs.writeFile(targetPath, fileContents, (error)=>{
+			if (error) throw error; 
+			res.json("Uploaded");
+
+			var insertQuery = "INSERT INTO images (imageUrl) VALUE (?)";
+			connection.query(insertQuery, [req.file.originalname], (dberror, results, fields)=>{
+				if (dberror) throw error; 
+				res.redirect('/?file="uploaded"');
+			})
+		});
+	});	
 });
 
 router.post('/addNewPlayer', (req, res, next)=>{
@@ -43,33 +75,49 @@ router.post('/addNewPlayer', (req, res, next)=>{
 });
 
 router.get('/testKnowledge/correctAnswer', (req, res, next)=>{
+	score++;
 	res.redirect('/testKnowledge');
 	
-	score++;
 	
+	
+})
+
+router.get('/testKnowledge/reset', (req, res, next)=>{
+	score = 0;
+	res.redirect('/testKnowledge');
 })
 
 router.get('/testKnowledge/wrongAnswer' ,(req, res, next)=>{
 	res.send('wrong');
 })
 
+router.get('testQ', (req, res, next)=>{
+	var query = 'SELECT * FROM images WHERE id > 2';
+	res.json(query);	
+})
+
+//landing page code for quiz lives below
 
 router.get('/testKnowledge', (req, res, next)=>{
 
 
 	// var getImagesQuery = "SELECT imageId FROM players_rankings";
 
+	// res.send(req.ip);
+
 	var selectQuery = "SELECT * FROM players_rankings";	
 	connection.query(selectQuery, (error, results, fields)=>{
 		// res.json(results);
 
-		var indexToPull = 4;
+		var indexToPull = Math.floor(10 * Math.random());
 		
 
 		var playerRanking = results[indexToPull].player_ranking;
 		//logic for wrong answer - generate a wrong wrank, if the player rank is equal to the wrong rank, generate another wrong rank
 		
 		var wrongRank =  Math.floor(10 * Math.random()) + 1;
+
+
 
 		while(wrongRank === playerRanking){
 			wrongRank =  Math.floor(10 * Math.random()) + 1;			
@@ -104,3 +152,15 @@ module.exports = router;
 // SELECT * FROM images WHERE id NOT IN 
 	// (SELECT imageID FROM votes WHERE ip= "::1")
 
+
+
+
+//looking for items between 
+
+// var id1=1;
+// var id2=3;
+
+// var query = "SELECT * FROM images WHERE id > ? AND id < ?"; 
+// connection.query(query, [id1, id2], (error, results, fields)=>{
+// 	res.json(results);
+// })
